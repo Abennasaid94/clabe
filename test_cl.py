c288c0ef6330a04d65bb276b8df16ba4c6df537e
Abennasaid94 <47219575+Abennasaid94@users.noreply.github.com>
2024-05-26 17:43:42 +0100

    Update test_cl.py

--- test_cl.py
+++ test_cl.py
@@ -1,4 +1,11 @@
 import socket,os,requests
+ip = requests.get('https://api.ipify.org').content.decode()
+print('My Ip NAT: %s'%ip)
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('8.8.8.8', 0))
+localip=s.getsockname()[0]
+s.close()
+print('My Local Ip: %s'%localip)
 listen_client_sock=socket.socket()
 ip,por='34.80.1.204',36234
 try:
